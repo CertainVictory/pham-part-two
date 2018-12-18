f0b571fbea2eab1c62025482e92e7994b1d1fbbb
tywin <certainvictorythp@gmail.com>
2018-12-18 12:58:14 -0500

    edited logic to match sample and accounted for capitalized answers

--- js/Script.js
+++ js/Script.js
@@ -4,56 +4,56 @@ function result() {
     var temp = "What temperature is it outside?";
     var tempFahr = prompt(temp);
     
-    if (tempFahr < 54 && eventType === 'casual') {
-        if (tempFahr < 54 && eventType === 'casual'){
-        console.log('wear a coat and something comfy');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a coat and something comfy.';}
+    if (tempFahr < 54 && eventType === 'casual' || eventType === 'Casual') {
+        if (tempFahr < 54 && eventType === 'casual' || eventType === 'Casual'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear something comfy and coat.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear something comfy and coat.';}
         
-    } else if (eventType === 'semi-formal'){
-        if (tempFahr < 54 && eventType === 'semi-formal'){
-        console.log('wear a coat and a polo');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a coat and a polo.';}
+    } else if (eventType === 'semi-formal' || eventType === 'Semi-formal'){
+        if (tempFahr < 54 && eventType === 'semi-formal' || eventType === 'Semi-formal'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a polo and coat.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a polo and coat.';}
     
-    } else if (eventType === 'formal'){
-        if (tempFahr < 54 && eventType === 'formal'){
-        console.log('wear a coat and a suit');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a coat and a suit.';}
+    } else if (eventType === 'formal' || eventType === 'Formal'){
+        if (tempFahr < 54 && eventType === 'formal' || eventType === 'Formal'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a suit and coat.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a suit and coat.';}
     }
-    
+  
     /*============================================*/
     
-    if (tempFahr > 54 && eventType === 'casual') {
-        if (tempFahr > 54 && tempFahr <=70 && eventType === 'casual'){
-        console.log('wear a jacket and something comfy');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a jacket and something comfy.';}
+    if (tempFahr >= 54 && eventType === 'casual' || eventType === 'Casual') {
+        if (tempFahr >= 54 && tempFahr <=70 && eventType === 'casual' || eventType === 'Casual'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear something comfy and jacket.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear something comfy and jacket.';}
         
-    } else if (tempFahr > 54 && eventType === 'semi-formal'){
-        if (tempFahr > 54 && tempFahr <=70 && eventType === 'semi-formal'){
-        console.log('wear a jacket and a polo');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a jacket and a polo.';}
+    } else if (tempFahr >= 54 && eventType === 'semi-formal' || eventType === 'Semi-formal'){
+        if (tempFahr >= 54 && tempFahr <=70 && eventType === 'semi-formal' || eventType === 'Semi-formal'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a polo and jacket.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a polo and jacket.';}
     
-    } else if (tempFahr > 54 && eventType === 'formal'){
-        if (tempFahr > 54 && tempFahr <=70 && eventType === formal){
-        console.log('wear a jacket and a suit');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a jacket and a suit.';}
+    } else if (tempFahr >= 54 && eventType === 'formal' || eventType === 'Formal'){
+        if (tempFahr >= 54 && tempFahr <=70 && eventType === 'formal' || eventType === 'Formal'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a suit and jacket.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a suit and jacket.';}
     }
     
     /*==============================================*/
     
-    if (tempFahr >= 70 && eventType === 'casual') {
-        if (tempFahr >= 70 && eventType === 'casual'){
-        console.log('something comfy and no jacket');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, just wear something comfy and no jacket.';}
+    if (tempFahr >= 70 && eventType === 'casual' || eventType === 'Casual') {
+        if (tempFahr > 70 && eventType === 'casual' || eventType === 'Casual'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear no jacket and something comfy.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear no jacket and something comfy.';}
         
-    } else if (tempFahr >= 70 && eventType === 'semi-formal'){
-        if (eventType === 'semi-formal'){
-        console.log('polo and no jacket');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a polo and no jacket.';}
+    } else if (tempFahr > 70 && eventType === 'semi-formal' || eventType === 'Semi-formal'){
+        if (eventType === 'semi-formal' || eventType === 'Semi-formal'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a polo and no jacket.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a polo and no jacket.';}
     
-    } else if (tempFahr >= 70 && eventType === 'formal'){
-        if (eventType === 'formal'){
-        console.log('suit and no jacket');
-        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, wear a suit and no jacket.';}
+    } else if (tempFahr > 70 && eventType === 'formal' || eventType === 'Formal'){
+        if (eventType === 'formal' || eventType === 'Formal'){
+        console.log('Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a suit and no jacket.');
+        text = 'Since it is ' + tempFahr + ' degrees outside and you going to a ' + eventType + ' event, you should wear a suit and no jacket.';}
     }
     
         document.getElementById("res").innerHTML = text;
