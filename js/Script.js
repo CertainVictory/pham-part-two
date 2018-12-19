4d6306654d9a34d2bf381a756865552bd782d314
tywin <certainvictorythp@gmail.com>
2018-12-19 12:07:34 -0500

    added missing logic and refactored

--- js/Script.js
+++ js/Script.js
@@ -20,49 +20,38 @@ function result() {
     var tempFahr = prompt(temp);
     let result = '';
         
-            if (tempFahr < 54 && eventType.toLowerCase() === 'casual'){
-            result = makeText(tempFahr, eventType, clothingEventType.casual) + ' a coat.';
-            console.log(result);}
-            if (tempFahr < 54 && eventType.toLowerCase() === 'semi-formal'){
-            result = makeText(tempFahr, eventType, clothingEventType['semi-formal']) + ' a coat.';
-            console.log(result);}
-             if (tempFahr < 54 && eventType.toLowerCase() === 'formal'){
-            result = makeText(tempFahr, eventType, clothingEventType.formal) + ' a coat.';
-            console.log(result);}
+        if (tempFahr < 54 && eventType.toLowerCase() === 'casual'){
+                result = makeText(tempFahr, eventType, clothingEventType.casual) + ' a coat.';
+                console.log(result);}
+        else if (tempFahr < 54 && eventType.toLowerCase() === 'semi-formal'){
+                result = makeText(tempFahr, eventType, clothingEventType['semi-formal']) + ' a coat.';
+                console.log(result);}
+        else if (tempFahr < 54 && eventType.toLowerCase() === 'formal'){
+                result = makeText(tempFahr, eventType, clothingEventType.formal) + ' a coat.';
+                console.log(result);}    
+        else if (tempFahr >= 54 && tempFahr <=70 && eventType.toLowerCase() === 'casual'){
+                result = makeText(tempFahr, eventType, clothingEventType.casual) + ' a jacket.';
+                console.log(result);}
+        else if (tempFahr >= 54 && tempFahr <=70 && eventType.toLowerCase() === 'semi-formal'){
+                result = makeText(tempFahr, eventType, clothingEventType['semi-formal']) + ' a jacket.';
+                console.log(result);}
+        else if (tempFahr >= 54 && tempFahr <=70 && eventType.toLowerCase() === 'formal'){
+                result = makeText(tempFahr, eventType, clothingEventType.formal) + ' a jacket.';
+                console.log(result);}
+        else if (tempFahr > 70 && eventType.toLowerCase() === 'casual'){
+                result = makeText(tempFahr, eventType, clothingEventType.casual) + ' no jacket.';
+                console.log(result);}
+        else if (tempFahr > 70 && eventType.toLowerCase() === 'semi-formal'){
+                result = makeText(tempFahr, eventType, clothingEventType['semi-formal']) + ' no jacket.';
+                console.log(result);}
+        else if (tempFahr > 70 && eventType.toLowerCase() === 'formal'){
+                result = makeText(tempFahr, eventType, clothingEventType.formal) + ' no jacket.';
+                console.log(result);}
+        else {
+                result = 'Try again';
+                console.log(result);}
             
             
-            
-    /*======================================*/
-    
-            
-            
-                    if (tempFahr >= 54 && tempFahr <=70 && eventType.toLowerCase() === 'casual'){
-            result = makeText(tempFahr, eventType, clothingEventType.casual) + ' a jacket.';
-            console.log(result);}
-            if (tempFahr >= 54 && tempFahr <=70 && eventType.toLowerCase() === 'semi-formal'){
-            result = makeText(tempFahr, eventType, clothingEventType['semi-formal']) + ' a jacket.';
-            console.log(result);}
-             if (tempFahr >= 54 && tempFahr <=70 && eventType.toLowerCase() === 'formal'){
-            result = makeText(tempFahr, eventType, clothingEventType.formal) + ' a jacket.';
-            console.log(result);}
-            
-            
-            
-    /*======================================*/
-            
-            
-            
-                    if (tempFahr > 70 && eventType.toLowerCase() === 'casual'){
-            result = makeText(tempFahr, eventType, clothingEventType.casual) + ' no jacket.';
-            console.log(result);}
-            if (tempFahr > 70 && eventType.toLowerCase() === 'semi-formal'){
-            result = makeText(tempFahr, eventType, clothingEventType['semi-formal']) + ' no jacket.';
-            console.log(result);}
-             if (tempFahr > 70 && eventType.toLowerCase() === 'formal'){
-            result = makeText(tempFahr, eventType, clothingEventType.formal) + ' no jacket.';
-            console.log(result);}
-            
-            
-                    document.getElementById("res").innerHTML = result;
+        document.getElementById("res").innerHTML = result;
     }
     
